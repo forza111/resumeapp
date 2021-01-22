8af3ef18a80ee7acee01401b27ba89b43f755d8a
forza111 <nikitos.ionkin@yandex.ru>
2021-01-22 09:29:05 +0300

    change views.py

--- resumeapp/viewinformation/views.py
+++ resumeapp/viewinformation/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'resumeapp/index-sidebar-dark-scheme.html')
+    return render(request, 'viewinformation/index-sidebar-dark-scheme.html')
 
 # Create your views here.
